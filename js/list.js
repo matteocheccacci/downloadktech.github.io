e400ae15a252c4c1a8d5282c5dbde250f15c796e
Matteo Checcacci <matteocheccacci@gmail.com>
2025-04-24 17:43:29 +0200

    push

--- js/list.js
+++ js/list.js
@@ -1,10 +1,10 @@
 const projectList = [
-{,
-name: "Progetto2",,
-icon: "/img/progetti/project2-icon.png",,
-zip: "404",,
-info: "https://downloads.kekkotech.com/projects/progetto2/progetto2.html",
-},
+  {
+    name: "Progetto2",
+    icon: "/img/progetti/project2-icon.png",
+    zip: "404",
+    info: "https://downloads.kekkotech.com/projects/progetto2/progetto2.html"
+  },
   {
     name: "Add Test",
     icon: "404",
