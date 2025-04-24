7297bfd79132f8ba9aea7bc9e99c4cfbf0935ee3
matteocheccacci <92668748+matteocheccacci@users.noreply.github.com>
2025-04-24 12:51:06 +0200

    Update list.js

--- js/list.js
+++ js/list.js
@@ -3,13 +3,13 @@ const projectList = [
     name: "AutoDesigna",
     icon: "/img/progetti/project1-icon.png",
     zip: "404",
-    info: "/projects/autodesigna/autodesigna.html"
+    info: "https://downloads.kekkotech.com/projects/autodesigna/autodesigna.html"
   },
   {
     name: "Progetto2",
     icon: "/img/progetti/project2-icon.png",
     zip: "404",
-    info: "/projects/progetto2/progetto2.html"
+    info: "https://downloads.kekkotech.com/projects/progetto2/progetto2.html"
   }
 ];
 
