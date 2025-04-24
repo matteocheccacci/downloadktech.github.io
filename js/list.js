98bce7960a3f2b4d5ae2b66afc254cd5abc4da6a
matteocheccacci <92668748+matteocheccacci@users.noreply.github.com>
2025-04-24 12:35:24 +0200

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,1 +1,16 @@
+// ./js/list.js
+const projectList = [
+  {
+    name: "Progetto1",
+    icon: "/img/progetti/project1-icon.png",
+    zip: "404",
+    info: "/projects/progetto1/progetto1.html"
+  },
+  {
+    name: "Progetto2",
+    icon: "/img/progetti/project2-icon.png",
+    zip: "404",
+    info: "/projects/progetto2/progetto2.html"
+  }
+];
 
