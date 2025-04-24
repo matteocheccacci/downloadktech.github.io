1ed2d3d6bfe1645c04a7eb95d5571701f67ee815
Matteo Checcacci <matteocheccacci@gmail.com>
2025-04-24 18:32:36 +0200

    Creato mioprogg

--- js/list.js
+++ js/list.js
@@ -1,1 +1,14 @@
-const projectList = [];
+const projectList = [
+  {
+    "name": "Mio Progetto",
+    "directoryName": "mioprogg",
+    "icon": "404",
+    "zip": "404",
+    "info": "https://downloads.kekkotech.com/projects/mioprogg/mioprogg.html",
+    "authors": "Admin",
+    "version": "1.0.0",
+    "size": "0.0 MB",
+    "description": "Mio progetto di prova",
+    "video": "404"
+  }
+];
---
+++ projects/mioprogg/mioprogg.js
@@ -0,0 +1,10 @@
+const projectData = {
+    title: "Mio Progetto",
+    description: "Mio progetto di prova",
+    version: "1.0.0",
+    size: "0.0 MB",
+    downloadLink: "404",
+    icon: "404",
+    video: "404",
+    authors: "Admin"
+  };
