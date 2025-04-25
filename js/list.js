9420ba37a0dac57fa47d0e082660bdc3a1df384f
Matteo Checcacci <matteocheccacci@gmail.com>
2025-04-26 01:35:25 +0200

    aggiunto progetto

--- js/list.js
+++ js/list.js
@@ -1,1 +1,14 @@
-const projectList = [];
+const projectList = [
+  {
+    "name": "Kekkotech Default Site",
+    "directoryName": "defaultktech",
+    "icon": "404",
+    "zip": "404",
+    "info": "https://downloads.kekkotech.com/projects/defaultktech/defaultktech.html",
+    "authors": "Matteo Checcacci",
+    "version": "5.1",
+    "size": "9.73 MB",
+    "description": "Sito in stile terminale (questo è un test).",
+    "video": "404"
+  }
+];
---
+++ projects/defaultktech/defaultktech.js
@@ -0,0 +1,10 @@
+const projectData = {
+    title: "Kekkotech Default Site",
+    description: "Sito in stile terminale (questo è un test).",
+    version: "5.1",
+    size: "9.73 MB",
+    downloadLink: "404",
+    icon: "404",
+    video: "404",
+    authors: "Matteo Checcacci"
+  };
