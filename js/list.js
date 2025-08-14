d5d84ecdbbc2417c93609120fda68e12c0faefc0
Matteo Checcacci <matteocheccacci@gmail.com>
2025-08-14 19:57:02 +0200

    removed test projects

--- js/list.js
+++ js/list.js
@@ -1,14 +1,1 @@
-const projectList = [
-  {
-    "name": "Kekkotech Default Site",
-    "directoryName": "defaultktech",
-    "icon": "ktechlogo.png",
-    "zip": "404",
-    "info": "https://downloads.kekkotech.com/projects/defaultktech/defaultktech.html",
-    "authors": "Matteo Checcacci",
-    "version": "0.1",
-    "size": "0.0",
-    "description": "Sito web a linea di comando.",
-    "video": "404"
-  }
-];
+const projectList = [];
